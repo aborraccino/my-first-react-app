619792e2d1a102118ae39acac25058c69f740fcc
Alessandro Borraccino <alessandro.borraccino@fisglobal.com>
2023-05-12 17:31:50 +0100

    first commit

---
+++ myapp/pages/index.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function Home() {
+
+  function ListItem({ints}) {
+    
+    return (
+      <>
+        {
+          ints.map(id => {
+            return <li>{id}</li>
+          })
+        }
+      </>
+    )
+  }
+
+  function ListItemWithState({ints}) {
+    const increment = 3;
+    return (
+      <>
+        <button onClick={() => addValue(increment) }>Add Item</button>
+        {
+          ints.map(id => {
+            return <li key={id}>{id}</li>
+          })
+        }
+      </>
+    )
+  }
+
+  const ints = [1, 2, 3, 4];
+  
+  const [ints2, setInts] = useState([1, 2, 3]);
+  function addValue(incrementValue){
+    const newVal = Math.max(...ints2) + incrementValue;
+    setInts([...ints2, newVal]);
+  }
+
+  return (
+    <div id="container">
+      <p>
+        List Items - static external params - no state
+        <ul>
+          <ListItem ints={ints}/>
+        </ul>
+      </p>
+      
+      <p>
+        List Items - useState
+        <ul>
+          <ListItemWithState ints={ints2} addValue={addValue}/>
+        </ul>
+      </p>
+    </div>
+  )
+}
